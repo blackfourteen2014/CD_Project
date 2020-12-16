3f3530a65d7c87f31d835b553986bda481c05929
kyeongmin-log <dotcom7860@gmail.com>
2020-12-16 14:48:06 +0900

    소스 정리

--- back/index.js
+++ back/index.js
@@ -54,19 +54,6 @@ app.use("/api/system", UserRouter);
 app.use("/api/system", CodeRouter);
 app.use("/api/system", HolidayRouter);
 
-//비밀번호 예시============================================================================================
-// const crypto = require('crypto');
-// const password = '123q';
-// const pass = crypto.createHash('sha512').update(password).digest('base64');
-// const pass2 = crypto.createHash('sha512').update(password).digest('base64');
-
-// if(pass === pass2){
-//   console.log('같다');
-// }else{
-//   console.log('다르다');
-// }
-//========================================================================================================
-
 //port number를 콘솔에 출력
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
